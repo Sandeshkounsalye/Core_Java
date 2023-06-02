b1a3ee583165fa62779467e60814a31d9be76606
Sandesh <sandeshpk411@gmail.com>
2023-06-02 16:55:07 +0530

    singleton

---
+++ src/SingletonDemo.java
@@ -0,0 +1,21 @@
+public class SingletonDemo {
+    private  static SingletonDemo singletonDemo=null;
+
+    public SingletonDemo(){
+    }
+
+    public static SingletonDemo getInstance() {
+        if (singletonDemo==null){
+            singletonDemo=new SingletonDemo();
+        }
+        return singletonDemo;
+    }
+
+    public static void main(String[] args) {
+        SingletonDemo s1=SingletonDemo.getInstance();
+        System.out.println(s1.hashCode());
+
+        SingletonDemo s2=SingletonDemo.getInstance();
+        System.out.println(s2.hashCode());
+    }
+}
